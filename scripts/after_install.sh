8c10c92e68c28c3743b6b405e5aa8426ab1cc43a
kaunghtet199499 <125899899+kaunghtet199499@users.noreply.github.com>
2023-03-04 13:51:25 +0630

    Add files via upload

---
+++ scripts/after_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /var/www/html/cms
+# npm install
+# npm install --save react react-dom react-scripts react-particles-js
+# npm install pm2 -g
+
+echo 'after install'
---
+++ scripts/before_install.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+# cd /home/ec2-user/server
+# curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+# yum -y install nodejs npm
+
+# this project was initialised manually before the pipeline. so, no need to setup npm and node stuffs manually
+echo 'before install'
